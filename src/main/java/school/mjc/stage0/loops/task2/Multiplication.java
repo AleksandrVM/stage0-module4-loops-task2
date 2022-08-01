35e3dff1d9982f7a0e370e8add80c614bd82227e
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-08-01 13:16:45 +0300

    solution Task #2. While Loops

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int i = 0;
+        int limit =  multiplyByAndToInclusive == 0 ? -1 : multiplyByAndToInclusive > 0 ? multiplyByAndToInclusive : -1 * multiplyByAndToInclusive;
+        while (i <= limit){
+            System.out.println(i*multiplyByAndToInclusive);
+            i++;
+        }
+    }
+
+    public static void main(String[] arvs){
+        new Multiplication().printMultiplied(-5);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 1;
+        int j = 1;
+        int counterPrime = 0;
+        while (i <= printToInclusive){
+            while (j <= i){
+                if (i % j == 0){
+                    counterPrime++;
+                }
+                j++;
+            }
+            if (counterPrime == 2)
+                System.out.println(i);
+            counterPrime = 0;
+            j = 1;
+            i++;
+        }
     }
 }
